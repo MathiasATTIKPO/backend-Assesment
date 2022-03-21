9021c1869b541bc8dcc7bf6627f248750b8ce450
Mathias ATTIKPO <etudiant_attikpo@outlook.fr>
2022-03-21 22:06:23 +0000

    3

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from asyncio.windows_events import NULL
 import os
+from urllib import response
 
 
 import uvicorn
-from fastapi import FastAPI , Request,  Response
+from fastapi import FastAPI , Request,  Response 
 import requests
-
-app = FastAPI(title="Backend Assessment")
-
 
 url = "https://api.hatchways.io/assessment/blog/posts?tag=tech"
 
@@ -32,35 +30,95 @@ def index(request:Request , response:Response):
         return {"success": False}
 
 @app.get('/api/posts')
-def post(  tags , sortBy , response:Response  ) :
+def post(  tags , sortBy , direction , response:Response  ) :
+    Posts = requests.get(url)
+    posts = Posts.json()
+    if direction == "ASC" or direction == "asc":
+        if sortBy == "id":
+            post_find = [post for post in posts if  tags.lower() in post["tags"].json()]
+            post_find.sort(key=lambda  x: x["id"] , reverse=True)
 
-    if sortBy == "ASC" or sortBy == "asc":
-        post_find = [post for post in posts if  tags.lower() in post["tags"]]
-        post_find.sort(key=lambda  x: x["id"] , reverse=True)
+            if not post_find:
+                response.status_code=404
+                return "Not Posts Found"
+            else:return post_find if len(post_find) >1 else post_find[0] 
+        
+        elif sortBy =="likes":
+            post_find = [post for post in posts if  tags.lower() in post[2]]
+            post_find.sort(key=lambda  x: x["likes"] , reverse=True)
 
-        if not post_find:
-            response.status_code=404
-            return "Not Posts Found"
-        else:return post_find if len(post_find) >1 else post_find[0]
-       # return post_find_sort
-    elif sortBy == "DESC" or sortBy == "desc":
-        post_find = [post for post in posts if  tags.lower() in post["tags"]]
-        post_find.sort(key=lambda  x: x["id"] , reverse=False)
+            if not post_find:
+                response.status_code=404
+                return "Not Posts Found"
+            else:return post_find if len(post_find) >1 else post_find[0]
+        
+        elif sortBy =="reads":
+            post_find = [post for post in posts if  tags.lower() in post[2]]
+            post_find.sort(key=lambda  x: x["reads"] , reverse=True)
 
-        if not post_find:
-            response.status_code=404
-            return "Not Posts Found"
-        else:return post_find if len(post_find) >1 else post_find[0]
-        #return post_find_sort
-    elif sortBy==" " or sortBy==NULL:
-        return {"error":"SortBy paramter not valid"}
-    else :
-        return {"error":"SortBy paramter not valid"}
+            if not post_find:
+                response.status_code=404
+                return "Not Posts Found"
+            else:return post_find if len(post_find) >1 else post_find[0]
+
+        elif sortBy =="popularity":
+            post_find = [post for post in posts if  tags.lower() in post[2]]
+            post_find.sort(key=lambda  x: x["popularity"] , reverse=True)
+
+            if not post_find:
+                response.status_code=404
+                return "Not Posts Found"
+            else:return post_find if len(post_find) >1 else post_find[0]
+            # return post_find_sort
+
+    elif direction == "DESC" or direction == "desc":
+            if sortBy == "id":
+                post_find = [post for post in posts if  tags.lower() in post[2]]
+                post_find.sort(key=lambda  x: x["id"] , reverse=True)
+
+                if not post_find:
+                    response.status_code=404
+                    return "Not Posts Found"
+                else:return post_find if len(post_find) >1 else post_find[0] 
+
+            elif sortBy =="likes":
+            
+                post_find = [post for post in posts if  tags.lower() in post[2]]
+                post_find.sort(key=lambda  x: x["likes"] , reverse=True)
+
+                if not post_find:
+                    response.status_code=404
+                    return "Not Posts Found"
+                else:return post_find if len(post_find) >1 else post_find[0]
+        
+            elif sortBy =="reads":
+                post_find = [post for post in posts if  tags.lower() in post[2]]
+                post_find.sort(key=lambda  x: x["reads"] , reverse=True)
+
+                if not post_find:
+                    response.status_code=404
+                    return "Not Posts Found"
+                else:return post_find if len(post_find) >1 else post_find[0]
+
+            elif sortBy =="popularity":
+                post_find = [post for post in posts if  tags.lower() in post[2]]
+                post_find.sort(key=lambda  x: x["popularity"] , reverse=True)
+
+                if not post_find:
+                    response.status_code=404
+                    return "Not Posts Found"
+                else:return post_find if len(post_find) >1 else post_find[0]
+                # return post_find_sort
+
+            elif sortBy==" " or sortBy==NULL:
+                return {"error":"direction paramter not valid"}
+            else :
+                return {"error":"direction paramter not valid"}
     
         
 
 
     #return {"request": request, "tag": tags}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    if __name__ == "__main__":
+        uvicorn.run(app, host="0.0.0.0", port=8000)
